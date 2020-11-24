ba48644553dbb10aaf6b22a29e6806ef863be8b2
esikkala <esko.ikkala@aalto.fi>
2020-11-24 15:40:22 +0200

    "Life situations" -> "Contextual search" etc.

--- src/client/translations/lawsampo/localeEN.js
+++ src/client/translations/lawsampo/localeEN.js
@@ -17,7 +17,7 @@ export default {
     info: {
       info: 'Info',
       blog: 'Project homepage',
-      blogUrl: 'https://seco.cs.aalto.fi/projects/lawlod',
+      blogUrl: 'https://seco.cs.aalto.fi/projects/lakisampo/',
       aboutThePortal: 'About the Portal'
     },
     searchBarPlaceHolder: 'Search all content',
@@ -569,15 +569,27 @@ export default {
       shortDescription: 'Document-based search for court decisions'
     },
     situations: {
-      label: '?Life Situations?',
-      mainPageLabel: '?Life Situations?',
-      shortDescription: 'Legislation and case law related to life situations'
+      label: 'Contextual search',
+      mainPageLabel: 'Contextual Search',
+      shortDescription: 'Contextual search for legislation and court decisions'
     }
   },
-  aboutThePortal: `
+  aboutThePortalPartOne: `
     <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
       About the Portal
     </h1>
+    <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+      LawSampo system consists of two components. 1) The LawSampo portal with its different application perspectives 
+      is targeted for human end-users for searching and exploring Finnish legislation and case law. 
+      2) The portal is based on the Linked Open  Data service Semantic Finlex that publishes data of the national 
+      Finlex service of the Ministry of Justice in Finland. The data service can be used freely for 
+      application development and is available at the Finnish Linked Data service LDF.fi.
+    </p>
+    <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
+      LawSampo is a living laboratory prototype on the Web. It has been developed by the Semantic Computing Research 
+      Group (SeCo) at various research projects with the Ministry of Justice and Edita Publishing Ltd in Aalto University 
+      and Helsinki Centre for Digital Humanities (HELDIG) at the University of Helsinki in collaboration.
+    </p>
   `,
   instructions: `
     <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
@@ -672,8 +684,5 @@ export default {
     <h1 class="MuiTypography-root MuiTypography-h2 MuiTypography-gutterBottom">
       Feedback
     </h1>
-    <p class="MuiTypography-root MuiTypography-body1 MuiTypography-paragraph">
-      This semantic portal has been developed by the research project...
-    </p>
     `
 }
